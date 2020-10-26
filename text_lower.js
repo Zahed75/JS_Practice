2b1cf4cb67f7d8d67bc3513a09036ffbedf4a85e
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-10-27 02:07:36 +0600

    done

--- text_lower.js
+++ text_lower.js
@@ -1,2 +1,15 @@
 var promise ="I PROMISE I WILL NEVER LEFT YOU ANY OF COST";
-console.log(promise.toLocaleLowerCase())
+console.log(promise.toLowerCase())
+// find the index value
+
+var name="i hate you i love you i hate you. baby dance in the floor i hate you";
+console.log(name.indexOf("baby"))
+
+//split function
+
+var best= "you are the light you are the color of my blood"
+console.log(best.split("color"))
+
+var test ="Bangladesh"
+
+console.log(test.indexOf("dosh"))
