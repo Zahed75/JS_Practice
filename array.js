a2894f403ffa0c05d2b476f7e451f889de82d1ce
Zahed75 <Zahedhasan.CS@gmail.com>
2020-10-27 05:39:57 -0700

    array created

---
+++ array.js
@@ -0,0 +1,16 @@
+var bazarList=["Alo","Potol","Mango","Suger","oil","Dal"]
+console.log(bazarList[3]);
+
+// pick up array element
+
+var bazarList=["Alo","Potol","Mango","Suger","oil","Dal"]
+
+var bendara=bazarList[3];
+console.log(bendara);
+// vlaue add in array
+
+bazarList[1]="Mula"
+console.log(bazarList);
+
+
+
