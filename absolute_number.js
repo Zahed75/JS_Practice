b75fc526b7ed821a182d8aac11cfe2384c175df9
Zahed75 <Zahedhasan.CS@gmail.com>
2020-10-27 03:33:10 -0700

    multiple conditions

--- absolute_number.js
+++ absolute_number.js
@@ -9,3 +9,19 @@ console.log(absoluteNum);
 var value=535.5656;
 var value1=Math.round(value);
 console.log(value1);
+
+//Celing Method
+var list =56.06644;
+var result= Math.ceil(list);
+console.log(result);
+
+//floor means komano joto kacha kachi value houk
+
+var num=5.999999;
+var num2 = Math.floor(num);
+console.log(num2);
+
+//random number
+
+var num2=Math.random()*20;
+console.log(num2);
---
+++ condtions.js
@@ -0,0 +1,65 @@
+var coffe = 20;
+if(coffe<30){
+    console.log("Life is better with Coffe");
+}
+else{
+    console.log("Give me Some Money");
+}
+
+// rent conditions with check operator
+
+var rent = 12;
+if(rent==12){
+    console.log("***amra jabo****");
+}
+else{
+    console.log("amra jabo na");
+}
+
+// not equal oprtr
+
+var price =10;
+if(price!=10){
+    console.log("Yeah Yeahs");
+
+}
+else{
+    console.log("NOOOOOOOOOOOOOOOOOOO");
+}
+
+//MULTIPULE CONDITIONS
+ var jobPaiso = true;
+ var savings = 300000;
+
+ if(jobPaiso==true && savings>200000){
+     console.log("Maiya Khujo");
+ }
+ else{
+     console.log("Tor kopale maiya nai");
+ }
+
+ // OR Conditions
+
+ var jobPaiso = true;
+ var savings = 300;
+
+ if(jobPaiso==true || savings>200000){
+     console.log("Maiya Khujo");
+ }
+ else{
+     console.log("Tor kopale maiya nai");
+ }
+
+ //Mukltiple conditions
+  var job= false;
+  var salary = 3000;
+
+  if(job==true && salary>10000){
+      console.log("mara kha");
+  }
+  else if(salary==3000){
+      console.log("OKh wait for marry");
+  }
+  else{
+      console.log("Shanti nai");
+  }
