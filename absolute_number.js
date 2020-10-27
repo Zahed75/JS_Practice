02598d0abe3bf517d55f9743d9536a5d0766c1e1
unknown <you@example.com>
2020-10-27 02:16:10 -0700

    absolute

---
+++ absolute_number.js
@@ -0,0 +1,11 @@
+//absolute number
+var num1= -45.545;
+
+var absoluteNum=Math.abs(num1);
+console.log(absoluteNum);
+
+//Round Number
+
+var value=535.5656;
+var value1=Math.round(value);
+console.log(value1);
--- data_types.js
+++ data_types.js
@@ -1,3 +1,60 @@
-var num1=54
+var num1=54.88
 var num2=434
 console.log(num1+num2);
+
+var digit1=545.454
+var digit2="3434";
+
+digit1=parseInt(digit1);
+
+console.log(digit1+digit2);
+// addition 2 values
+var num1=34;
+var num2 =56;
+var total=num1+num2;
+console.log(total)
+
+// subtraction 2 values
+var num1=34;
+var num2 =56;
+var total=num1-num2;
+console.log(total)
+
+// multipluction 2 vlaues
+var num1=34;
+var num2 =56;
+var total=num1*num2;
+console.log(total)
+
+// dividie 2 vlaues
+var tst1= 23;
+var tst2 =2;
+ var result= tst1 % tst2;
+ console.log(result);
+
+ //increment
+
+ var price1=3453;
+ var price2= 343;
+
+ //price2=price2+1;
+
+ price2--; // seconde method of vlaue Decincrement
+ price2++;//increment
+ 
+ console.log(price2)
+
+ // strings vlaue
+
+var name="Promi";
+var num1 =22;
+ var hate= name+' '+num1;
+ console.log(hate)
+
+
+var fName="Zahed";
+var l_name="Hasan"
+console.log(fName+' '+l_name);
+
+
+
